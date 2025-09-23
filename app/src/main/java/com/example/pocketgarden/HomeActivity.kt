ed1b504586992b3014d11b90e024337cd6d3290b
Kiana Pillay <ST10258543@vcconnect.edu.za>
2025-09-23 15:07:52 +0200

    Added HomeActivity and updated get started button on Home Page

---
+++ app/src/main/java/com/example/pocketgarden/HomeActivity.kt
@@ -0,0 +1,22 @@
+package com.example.pocketgarden
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+
+        if(savedInstanceState == null)
+        {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container,HomePageFragment())
+                .commit()
+        }
+    }
+}
