a6d80255e943f509d6ee0311eb4bea95684a7006
Kiana Pillay <ST10258543@vcconnect.edu.za>
2025-09-29 09:55:23 +0200

    Added update profile screen and navigation

--- app/src/main/java/com/example/pocketgarden/EditProfileFragment.kt
+++ app/src/main/java/com/example/pocketgarden/EditProfileFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SettingsPageFragment.newInstance] factory method to
+ * Use the [EditProfileFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SettingsPageFragment : Fragment() {
+class EditProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,26 +34,8 @@ class SettingsPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings_page, container, false)
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        //Navigation button
-        val updateProfileBtn = view.findViewById<Button>(R.id.button24)
-
-        updateProfileBtn.setOnClickListener {
-            // Navigate to EditProfileFragment
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, EditProfileFragment())
-                .addToBackStack(null)
-                .commit()
-        }
-    }
-
-
-
 
     companion object {
         /**
@@ -63,12 +44,12 @@ class SettingsPageFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsPageFragment.
+         * @return A new instance of fragment EditProfileFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SettingsPageFragment().apply {
+            EditProfileFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
